87674680693d7e9c53247fb3c1f2f9c62856ee52
Yuri-Gabriel <yurigabriel.f1012@gmail.com>
2026-05-10 20:40:13 -0300

    start

---
+++ lex/lex.h
@@ -0,0 +1,7 @@
+
+#include <stdio.h>
+
+void test_lexico() {
+    printf("Ypsilon - Lexico\n");
+    printf("========================================================\n");
+}
---
+++ main.c
@@ -0,0 +1,6 @@
+#include "lex/lex.h"
+
+int main() {
+    test_lexico();
+    return 0;
+}
